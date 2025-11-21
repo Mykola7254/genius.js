14e89463d9e3f46f58e96e97b60fe5f901db29d4
Mukola <122.24trusevych.m@nltu.lviv.ua>
2025-11-21 15:42:41 +0200

    learn methods arr

---
+++ Lesson 3/script3.js
@@ -0,0 +1,59 @@
+// 1
+// const fruts = [
+//   { id: 0, name: 'Apple' },
+//   { id: 1, name: 'Tomat' },
+//   { id: 2, name: 'Cherry' },
+//   { id: 3, name: 'Orange' },
+// ];
+
+// const names = fruts.map(item => item.name);
+
+// console.log(names);
+
+// 2
+// let a = 2;
+// for (a; a <= 10; a += 2) {
+//   console.log(a);
+// }
+
+// 3
+// for (let i = 0; i < 5; i++) {
+//      console.lpg( `цифра ${i}!` );
+// }
+
+// let i = 0;
+// while (i < 5) {
+//   i++;
+//   console.log(`цифра ${i}!`);
+// }
+
+// 4
+// let number;
+
+// while (true) {
+//   number = prompt('Введіть число більше за 100:');
+
+//   // якщо користувач натиснув "скасувати" або ввів порожньо — виходимо
+//   if (number === null || number === '') {
+//     break;
+//   }
+
+//   // перетворюємо на число і перевіряємо
+//   if (+number > 100) {
+//     alert('Молодець!');
+//     break;
+//   }
+// }
+
+// 5
+// const girls = [
+//   { age: 23, name: 'Оля' },
+//   { age: 29, name: 'Аня' },
+//   { age: 10, name: 'Юля' },
+//   { age: 20, name: 'Катя' },
+// ];
+
+// const averageAge =
+//   girls.reduce((sum, girl) => sum + girl.age, 0) / girls.length;
+
+// console.log(averageAge);
